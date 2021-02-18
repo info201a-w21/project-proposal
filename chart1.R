b9fcd504dee0e4583ce1c4b2593f9126fe0c3eae
Audrey Dennis <audreydennis@Audreys-MacBook-Air-3.local>
2021-02-18 14:02:01 -0800

    Create rmd file and chart 1

---
+++ chart1.R
@@ -0,0 +1,23 @@
+library(stringr)
+library(tidyverse)
+library(countrycode)
+
+covid <- read.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
+
+covid_data <- covid %>%
+  filter(date== max(date)) %>%
+  select(iso_code, location, total_cases)
+
+# most_recent_date <- covid_data %>%
+#   filter(date == avg(date))
+
+shapefile <-map_data("world")
+  
+shapefile <- shapefile %>%
+  mutate(iso_code = countrycode(region, origin = 'country.name', destination = 'iso3c')) %>%
+  left_join(covid_data, by = "iso_code")
+
+covid_map <- ggplot(data = shapefile)+
+  geom_polygon(mapping = aes(x = long, y = lat, group = group, fill = total_cases))+
+  labs(title = paste("Total Number of Covid Cases in Each Country"), 
+       x = "", y = "", fill = "Number of Cases")
