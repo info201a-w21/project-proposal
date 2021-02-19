5c589cdcb5a2991c8cb6790d92295188276b3a59
thomasjosephwilson <60676608+thomasjosephwilson@users.noreply.github.com>
2021-02-19 00:21:52 +0000

    fixing thomas' chart..

--- chart2.R
+++ chart2.R
@@ -1,65 +1,43 @@
 library(dplyr)
 library(ggplot2)
-library(ggrepel) #if you're running this you will need to install the package
+
+owid.covid.data <- read.csv("owid-covid-data.csv")
+View(owid.covid.data)
 
 #gdp per capita for each country
+
 country_and_GDP <- owid.covid.data %>% 
-select(V3, V50) %>% #only take country name and GDP
+select(location, gdp_per_capita) %>% #only take country name and GDP
   unique() %>% #dont need repeats
-  filter(V50 != "") %>%  #remove rows/countries with no GDP listed
-  filter(V50 != "gdp_per_capita") #removing text bc it shows up on graph
+  filter(gdp_per_capita != "") %>% 
+  filter(gdp_per_capita != "World")
 country_and_GDP
-
-#converting to numeric
-
-is.numeric(country_and_GDP$V50) #FALSE. what? why is it a string?
-country_and_GDP$V50 <- as.numeric(country_and_GDP$V50) #converting to numeric.
-country_and_GDP$V50 
-is.numeric(country_and_GDP$V50) #TRUE. 
-
-
-#converting to numeric
-
 
 #deaths for each country
 country_and_DEATHS <- owid.covid.data %>% 
-  select(V3, V8) %>%  #only take country name and cases
-  filter(V8 != "") %>%  #filter out countries with no cases listed
-  filter(V8 != "total_deaths")
-country_and_DEATHS
-
-#sorry for the split here, something is wonky - dataset cases should be a number but instead its a stirng
-
-is.numeric(country_and_DEATHS$V8) #FALSE. what? why is it a string?
-country_and_DEATHS$V8 <- as.numeric(country_and_DEATHS$V8) #converting to numeric.
-country_and_DEATHS$V8 
-is.numeric(country_and_DEATHS$V8) #TRUE. 
+  select(location, total_deaths) %>%  #only take country name and cases
+  filter(total_deaths != "") %>% 
 country_and_DEATHS
 
 country_and_DEATHS <-  country_and_DEATHS %>% 
-  group_by(V3) %>% 
-  filter(V8 == max(V8)) %>% #filter to see the max cases for each location
+  group_by(location) %>% 
+  filter(total_deaths == max(total_deaths )) %>% #filter to see the max cases for each location
   unique()   #some of the data never changes (e.g. dominican republic had the same amount of cases for 20 rows)
 country_and_DEATHS
-
 
 
 #joining both
 capita_to_deaths <- left_join(
   country_and_DEATHS,
   country_and_GDP,
-  by = "V3")
+  by = "location")
 
 #cleaning up the data a bit more
 capita_to_deaths <- capita_to_deaths %>% 
-  na.omit() %>% #removing NA values, in turn removing entire continents (e.g. Africa, Asia) 
-filter(V3 != "World") #removing world, will only confuse if its included in graph.
-  capita_to_deaths
+  na.omit() #removing NA values, in turn removing entire continents (e.g. Africa, Asia) 
 
-#changing names
-names(capita_to_deaths)[1] <- "location"
-names(capita_to_deaths)[2] <- "total_deaths"
-names(capita_to_deaths)[3] <- "gdp_per_capita"
+capita_to_deaths <- filter(V3 != "World") #removing world, will only confuse if its included in graph.
+  capita_to_deaths
 
 
 #making graph
@@ -70,7 +48,6 @@ covid_plot_2 <- ggplot(data = capita_to_deaths) +
   scale_x_continuous(limits = c(4000, 70000)) + #show those countries with atleast 10k gdp
   scale_y_continuous(limits = c(1000,59000)) +
   labs(title="Relationship Between Countries Death Rates and GDP", subtitle = "The blue line represents average total deaths") +
-
   xlab("GDP Per Capita") +
   ylab("Total Deaths") 
   
