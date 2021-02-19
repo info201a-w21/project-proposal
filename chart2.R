f27f104874cbe4f2460b021460c22eeefe4a0dad
thomasjosephwilson <60676608+thomasjosephwilson@users.noreply.github.com>
2021-02-19 12:44:50 -0800

    uploading thomas' part to index.html ..

--- chart2.R
+++ chart2.R
@@ -1,5 +1,7 @@
 library(dplyr)
 library(ggplot2)
+library(stringr)
+library(tidyverse)
 
 owid.covid.data <- read.csv("owid-covid-data.csv")
 View(owid.covid.data)
@@ -9,21 +11,16 @@ View(owid.covid.data)
 country_and_GDP <- owid.covid.data %>% 
 select(location, gdp_per_capita) %>% #only take country name and GDP
   unique() %>% #dont need repeats
-  filter(gdp_per_capita != "") %>% 
-  filter(gdp_per_capita != "World")
-country_and_GDP
+  filter(gdp_per_capita != "") 
 
 #deaths for each country
 country_and_DEATHS <- owid.covid.data %>% 
   select(location, total_deaths) %>%  #only take country name and cases
   filter(total_deaths != "") %>% 
-country_and_DEATHS
-
-country_and_DEATHS <-  country_and_DEATHS %>% 
   group_by(location) %>% 
   filter(total_deaths == max(total_deaths )) %>% #filter to see the max cases for each location
   unique()   #some of the data never changes (e.g. dominican republic had the same amount of cases for 20 rows)
-country_and_DEATHS
+
 
 
 #joining both
@@ -36,9 +33,6 @@ capita_to_deaths <- left_join(
 capita_to_deaths <- capita_to_deaths %>% 
   na.omit() #removing NA values, in turn removing entire continents (e.g. Africa, Asia) 
 
-capita_to_deaths <- filter(V3 != "World") #removing world, will only confuse if its included in graph.
-  capita_to_deaths
-
 
 #making graph
 covid_plot_2 <- ggplot(data = capita_to_deaths) +
@@ -50,5 +44,3 @@ covid_plot_2 <- ggplot(data = capita_to_deaths) +
   labs(title="Relationship Between Countries Death Rates and GDP", subtitle = "The blue line represents average total deaths") +
   xlab("GDP Per Capita") +
   ylab("Total Deaths") 
-  
-  covid_plot_2
