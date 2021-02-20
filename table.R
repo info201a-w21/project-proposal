107623b0a5819d213827c2e87dda578823f67cf4
xpnxix <xpnxix88@uw.edu>
2021-02-19 21:15:54 -0800

    Added table, heat map desc, final paragraph

---
+++ table.R
@@ -0,0 +1,41 @@
+
+library(tidyverse)
+library(stringr)
+library(lintr)
+data <- read.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
+
+# Aggregate table ---------------------------------------------------------
+
+highest_stringency_countries <- data %>% 
+  filter(stringency_index == max(stringency_index, na.rm = TRUE)) %>% 
+  group_by(location) %>% 
+  summarize(new_cases = mean(new_cases, na.rm = TRUE)) 
+
+
+# Rename columns ----------------------------------------------------------
+
+ highest_stringency_countries <- highest_stringency_countries %>%
+  rename(avg_daily_cases = new_cases)
+
+ highest_stringency_countries <- highest_stringency_countries %>% 
+   rename(stringent_countries = location)
+ 
+
+# Round average new cases -------------------------------------------------
+ 
+ highest_stringency_countries <- highest_stringency_countries %>% 
+   mutate_if(is.numeric, round, digits = 0)
+
+
+# Create table ------------------------------------------------------------
+ 
+ kable(highest_stringency_countries)
+ 
+ 
+# -------------------------------------------------------------------------
+
+ 
+
+  
+  
+  
